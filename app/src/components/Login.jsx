03fe34199d8f38c04e77f58da3de6392f8702a5e
Darren Richardson <antoniounivers@gmail.com>
2019-03-13 19:56:16 -0400

    pulled form master

--- app/src/components/Login.jsx
+++ app/src/components/Login.jsx
@@ -1,24 +1,84 @@
 import React, { useState } from 'react';
 
+import styled from 'styled-components';
+
+const LoginContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  margin: 0 auto;
+`;
+
+const LoginForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  width: 60%;
+`;
+
+const InputWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  width: 100%;
+
+  > input {
+    width: 100%;
+    border: 1px solid black;
+    border-radius: 3px;
+    margin: 10px 0;
+    height: 30px;
+    padding: 0 10px;
+  }
+`;
+
+const BtnWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+
+  > button {
+    width: 45%;
+    border: 1px solid black;
+    border-radius: 3px;
+    margin: 10px 0;
+    height: 30px;
+    padding: 0 10px;
+    background: white;
+    color: black;
+
+    :first-child {
+      background-color: black;
+      color: white;
+    }
+  }
+`;
+
 const Login = props => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   return (
-    <div>
-      <form>
-        <input
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={e => setUsername(e.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="Password"
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-        />
-        <div>
+    <LoginContainer>
+      <LoginForm>
+        <InputWrapper>
+          <input
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={e => setUsername(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
+        </InputWrapper>
+        <BtnWrapper>
           <button
             onClick={e => {
               e.preventDefault();
@@ -28,7 +88,7 @@ const Login = props => {
             }}
             type="submit"
           >
-            Submit
+            Login
           </button>
           {!props.signingUp && (
             <button
@@ -37,12 +97,12 @@ const Login = props => {
                 props.history.push('/signup');
               }}
             >
-              Register
+              Sign Up
             </button>
           )}
-        </div>
-      </form>
-    </div>
+        </BtnWrapper>
+      </LoginForm>
+    </LoginContainer>
   );
 };
 
