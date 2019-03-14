682a0416262715071d157d7dc2df73b56ee19e94
jjgorton <jjgorton@gmail.com>
2019-03-14 05:16:55 -0400

    greensock animation added, sticky button made

--- landing/index.js
+++ landing/index.js
@@ -1,3 +1,4 @@
+//Mobile/desktop menu
 const logo = document.querySelector('.mobile-menu');
 const navBar = document.querySelector('nav');
 
@@ -5,12 +6,62 @@ logo.addEventListener('click', function () {
     navBar.classList.toggle('navOn');
 });
 
-const button = document.querySelector('button');
 
-button.addEventListener('mousedown', function () {
-    button.classList.add('buttonClick');
+//button clicks
+const button = document.querySelectorAll('button');
+
+button.forEach(function (click) {
+    click.addEventListener('mousedown', function () {
+        click.classList.add('buttonClick');
+    });
+
+    click.addEventListener('mouseup', function () {
+        click.classList.remove('buttonClick');
+    });
 });
 
-button.addEventListener('mouseup', function () {
-    button.classList.remove('buttonClick');
-});
+
+//slogan animation
+
+window.onload = function () {
+    const slogan1 = document.querySelector(".slogan1");
+    const slogan2 = document.querySelector(".slogan2");
+    const button = document.querySelector('.sign-up');
+    // TweenLite.to(slogan, 1, {
+    //     left: "632px"
+    // });
+
+    TweenLite.from(slogan1, 4, {
+        opacity: 0,
+        left: "500px"
+    });
+
+    TweenLite.from(slogan2, 4, {
+        opacity: 0,
+        left: "500px",
+        delay: 2
+    });
+
+    TweenLite.from(button, 7, {
+        opacity: 0,
+        delay: 5
+    });
+}
+
+//css position sticky won't work for multiple reasons... this hack does!
+const buttonNav = document.querySelector('.navButton');
+const buttonSlogan = document.querySelector('.sign-up');
+
+window.onscroll = function () {
+    stickyButton()
+};
+
+function stickyButton() {
+    if (document.body.scrollTop > 550 || document.documentElement.scrollTop > 550) {
+        buttonNav.style.visibility = "visible";
+        buttonSlogan.style.visibility = "hidden";
+    } else {
+        buttonNav.style.visibility = "hidden";
+        buttonSlogan.style.visibility = "visible";
+    }
+}
