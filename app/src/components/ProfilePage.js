002013fe3713f049ac1b0eaa64fd5aeb76d43afb
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-03-14 03:42:00 -0400

    show recent likes on ProfilePage

--- app/src/components/ProfilePage.js
+++ app/src/components/ProfilePage.js
@@ -1,11 +1,44 @@
 import React, {Component} from 'react'
-import RadarChart from './RadarChart'
 import {connect} from 'react-redux'
 import {withRouter} from 'react-router-dom'
-import {logout} from '../actions'
+import axios from 'axios'
+
+import RadarChart from './RadarChart'
+import Track from './Track'
+
+import {logout, getTracks} from '../actions'
 
 
 class ProfilePage extends Component{
+    constructor(props){
+        super(props)
+
+        this.state = {
+            fetchingLikedTracks: false,
+            likedTracksWithSpotifyData: []
+        }
+    }
+
+    componentDidMount(){
+        if(this.props.likedTracks) {
+            this.getLikedTracks()
+        }
+    }
+
+    getLikedTracks = () => {
+        this.setState({
+            fetchingLikedTracks: true,
+            likedTracksWithSpotifyData: []
+        })
+        let trackIds = (this.props.likedTracks).map(track => track.track_id).join(',')
+        this.props.getTracks(trackIds, this.props.accessToken).then(res => {
+            this.setState({
+                fetchingLikedTracks: false,
+                likedTracksWithSpotifyData: res.data.tracks
+            })
+        })
+    }
+
 
     handleLogOut = () => {
         this.props.history.push('/')
@@ -17,6 +50,9 @@ class ProfilePage extends Component{
                 {this.props.userMusicTaste && <RadarChart audioFeatures = {[this.props.userMusicTaste]}/>}
                 <h1>You</h1>
                 <button onClick = {this.handleLogOut}>Signout</button>
+                <p>most recent likes</p>
+                {this.state.fetchingLikedTracks && <p>fetching liked tracks</p>}
+                {this.state.likedTracksWithSpotifyData.map(track => <Track key = {track.id} trackData = {track}/>)}
             </>
         )
     }
@@ -25,11 +61,9 @@ class ProfilePage extends Component{
 const mstp = state => {
     return {
         likedTracks: state.auth.userData.likedTracks,
-        userMusicTaste: state.auth.userMusicTaste
+        userMusicTaste: state.auth.userMusicTaste,
+        accessToken: state.track.accessToken
     }
 }
 
-export default withRouter(connect(mstp, {logout})(ProfilePage))
-
-
-
+export default withRouter(connect(mstp, {logout, getTracks})(ProfilePage))
