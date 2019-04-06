2a70ac77311a57cc9db568c63e21156a6b769198
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-04-06 13:44:01 -0400

    Refactored LoginPage
    for readability

--- app/src/components/LoginPage/LoginPage.js
+++ app/src/components/LoginPage/LoginPage.js
@@ -10,66 +10,67 @@ import LoadingScreen from '../minorComps/LoadingScreen';
 import Logo from '../minorComps/Logo';
 
 class LoginPage extends Component {
-  constructor(props){
-    super(props);
 
-    this.state = {
-      email: '',
-      password: '',
-      login: true
-    };
-  }
+		state = {
+			email: '',
+			password: '',
+			login: true
+		};
 
-    onChange = e => {
-      this.setState({
-        [e.target.name]: e.target.value
-      });
-    }
+		render(){
+			return(
+				<>
+					<LoginForm onSubmit = {e => e.preventDefault()}>
+						<Logo/>
+						<input name = 'email' type='text' value = {this.state.email} placeholder = 'email' onChange = {this.onChange}/>
+						<input name = 'password' type='password' value = {this.state.password} placeholder = 'password' onChange = {this.onChange}/>
+						{this.state.login ? 
+							<AuthButton name = 'login' type = 'submit' value = 'login' onClick = {this.handleButtonClick}/>:
+							<AuthButton name = 'signup' type = 'submit' value = 'signup' onClick = {this.handleButtonClick}/> 
+						}
+						<p onClick = {this.handleSwitch} style = {{cursor: 'pointer'}}>{this.state.login ? 'SignUp' : 'Login'}</p>
+					</LoginForm>
+					{this.props.authenticating && <LoadingScreen/>}
+				</>
+			);
+		}
 
-    clearUsernamePassword = () => {
-      this.setState({
-        email: '',
-        password: '',
-        login: true
-      });
-    }
+		onChange = e => {
+			this.setState({
+				[e.target.name]: e.target.value
+			});
+		}
 
-    handleButtonClick = e => {
-      if(e.target.name === 'login'){
-        this.props.authenticate(this.state.email, this.state.password);
-      }else{
-        this.props.signup(this.state.email, this.state.password);
-      }
-      this.clearUsernamePassword();   
-    }
-    handleSwitch = e => {
-      this.setState({
-        login: !this.state.login
-      });
-    }
-    render(){
-      return(
-            <>
-            <LoginForm onSubmit = {e => e.preventDefault()}>
-              <Logo/>
-              <input name = 'email' type='text' value = {this.state.email} placeholder = 'email' onChange = {this.onChange}/>
-              <input name = 'password' type='password' value = {this.state.password} placeholder = 'password' onChange = {this.onChange}/>
-              {this.state.login ? 
-                <AuthButton name = 'login' type = 'submit' value = 'login' onClick = {this.handleButtonClick}/>:
-                <AuthButton name = 'signup' type = 'submit' value = 'signup' onClick = {this.handleButtonClick}/> 
-              }
-              <p onClick = {this.handleSwitch} style = {{cursor: 'pointer'}}>{this.state.login ? 'SignUp' : 'Login'}</p>
-            </LoginForm>
-            {this.props.authenticating && <LoadingScreen/>}
-            </>
-      );
-    }
+		clearUsernamePassword = () => {
+			this.setState({
+				email: '',
+				password: '',
+				login: true
+			});
+		}
+
+		handleButtonClick = e => {
+			if(e.target.name === 'login'){
+				this.props.authenticate(this.state.email, this.state.password);
+			}else{
+				this.props.signup(this.state.email, this.state.password);
+			}
+			this.clearUsernamePassword();   
+		}
+		
+		handleSwitch = e => {
+			this.setState({
+				login: !this.state.login
+			});
+		}
+
+		
 }
 
 const mstp = state => {
-  return {
-    authenticating: state.auth.authenticating
-  };
+	return {
+		authenticating: state.auth.authenticating
+	};
 };
 
 export default connect(mstp, {authenticate, signup})(LoginPage);
