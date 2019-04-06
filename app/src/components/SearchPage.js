4066d35142be93a7291bb975c83f7d5879c7d8ec
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-04-06 14:25:21 -0400

    Refactored SearchPage.js
    For readability

--- app/src/components/SearchPage.js
+++ app/src/components/SearchPage.js
@@ -2,61 +2,59 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {debounce} from 'underscore';
 
-import {searchTrack, testToken} from '../actions';
 import Track from './Track';
 import {SongsContainer, SearchBar, Illustration} from './StyledComps';
 
 import gogglesIllustration from '../illustrations/goggles.png';
 
+import {searchTrack, testToken} from '../actions';
+
 class SearchPage extends Component {
-  constructor(props){
-    super(props);
 
-    this.state = {
-      searchTerm: ''
-    };
-  }
+	state = {
+		searchTerm: ''
+	};
 
-    onChange = e => {
-      this.setState({
-        [e.target.name]: e.target.value
-      });
+	render(){
+		return (
+			<div>
+				<SearchBar onSubmit = {e => e.preventDefault()}>
+					<input  type='text' 
+						name='searchTerm'
+						placeholder = 'Search Song'
+						value = {this.state.searchTerm}
+						onChange = {this.onChange}/>    
+				</SearchBar>
+				<SongsContainer>
+					{this.props.searchResults.map(track => <Track key = {track.id} trackData = {track}/>)}
+				</SongsContainer>
+				{this.props.searchResults.length === 0 && 
+							<div style = {{marginTop: '30px'}}>
+								<Illustration src={gogglesIllustration} alt="Goggles Illustration"/>
+								<p>Get Searchin</p>
+							</div> }
+			</div>
+		);
+	}
 
-      this.debounceSearchTrack(e.target.value);
-    }
+	onChange = e => {
+		this.setState({
+			[e.target.name]: e.target.value
+		});
 
-    debounceSearchTrack = debounce(value => {
-      this.props.searchTrack(value, this.props.accessToken);
-    }, 1000)
+		this.debounceSearchTrack(e.target.value);
+	}
 
-    render(){
-      return (
-        <div>
-          <SearchBar onSubmit = {e => e.preventDefault()}>
-            <input  type='text' 
-              name='searchTerm'
-              placeholder = 'Search Song'
-              value = {this.state.searchTerm}
-              onChange = {this.onChange}/>    
-          </SearchBar>
-          <SongsContainer>
-            {this.props.searchResults.map(track => <Track key = {track.id} trackData = {track}/>)}
-          </SongsContainer>
-          {this.props.searchResults.length === 0 && 
-                <div style = {{marginTop: '30px'}}>
-                  <Illustration src={gogglesIllustration} alt="Goggles Illustration"/>
-                  <p>Get Searchin</p>
-                </div> }
-        </div>
-      );
-    }
+	debounceSearchTrack = debounce(value => {
+		this.props.searchTrack(value, this.props.accessToken);
+	}, 1000)
 }
 
 const mstp = state => {
-  return {
-    accessToken: state.track.accessToken,
-    searchResults: state.track.searchResults
-  };
+	return {
+		accessToken: state.track.accessToken,
+		searchResults: state.track.searchResults
+	};
 };
 
 export default connect(mstp, {searchTrack, testToken})(SearchPage);
