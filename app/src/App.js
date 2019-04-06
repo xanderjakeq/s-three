a57083e3d57cd49474ea87dcfb22d65b2f18d697
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-04-06 15:57:16 -0400

    Refactored App.js
    For readability

--- app/src/App.js
+++ app/src/App.js
@@ -1,47 +1,43 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 
 import firebase from './firebaseApp'
 import LoginPage from './components/LoginPage/LoginPage';
 import MainApp from './components/MainApp/MainApp';
-
-import { AppContainer } from './components/StyledComps';
+import {AppContainer} from './components/StyledComps';
 
 import {getUserData, saveFirebaseUser} from './actions/authActions'
 
 class App extends Component {
-  
-  constructor(props){
-    super(props)
-    this.state = {
-      isAuthed: false
-    }
-  }
-  
-  componentDidMount(){
-    firebase.auth().onAuthStateChanged(user => {
-        if (user) {
-          this.props.saveFirebaseUser(user)
-          this.props.getUserData(user)
-          this.setState({
-            isAuthed: true
-          })
-        } else {
-          this.setState({
-            isAuthed: false
-          })
-        }
-    });
-  }
+	
+	state = {
+		isAuthed: false
+	}
 
-  render() {
-    return (
-      <AppContainer>
-        {this.state.isAuthed ? <MainApp/> : <LoginPage/>}
-      </AppContainer>
-    );
-  }
+	render() {
+		return (
+			<AppContainer>
+				{this.state.isAuthed ? <MainApp/> : <LoginPage/>}
+			</AppContainer>
+		);
+	}
+	
+	componentDidMount(){
+		firebase.auth().onAuthStateChanged(user => {
+				if (user) {
+					this.props.saveFirebaseUser(user)
+					this.props.getUserData(user)
+					this.setState({
+						isAuthed: true
+					})
+				} else {
+					this.setState({
+						isAuthed: false
+					})
+				}
+		});
+	}
 }
 
 export default connect(null, {getUserData, saveFirebaseUser})(App);
