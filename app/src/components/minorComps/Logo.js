fb26ec600afb6031a751a6ebd5d02aafe10d9cef
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-04-06 13:54:57 -0400

    Refactored Logo.js
    indentation

--- app/src/components/minorComps/Logo.js
+++ app/src/components/minorComps/Logo.js
@@ -3,7 +3,7 @@ import React from 'react';
 import logo from '../../s3Logo.svg';
 
 export default () => {
-  return (
-    <img src={logo} alt="Logo" style= {{width: '50px', margin: '10px'}}/>
-  );
+	return (
+		<img src={logo} alt="Logo" style= {{width: '50px', margin: '10px'}}/>
+	);
 };
