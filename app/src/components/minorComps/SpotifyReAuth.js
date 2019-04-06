2bad240c1c4d9ecd12da8a378c9146fc0b2157b8
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-04-06 14:03:14 -0400

    Spaces  to tabs

--- app/src/components/minorComps/SpotifyReAuth.js
+++ app/src/components/minorComps/SpotifyReAuth.js
@@ -5,13 +5,13 @@ import {FullScreenOverlay, Illustration} from '../StyledComps';
 import swirl from '../../illustrations/swirl.png';
 
 const SpotifyReAuth = (props) => {
-  return(
-    <FullScreenOverlay>
-      <Illustration src = {swirl}/>
-      <h3>Spotify</h3>
-      <a href = {'https://damp-island-94750.herokuapp.com/login' || 'https://spotify-ss.firebaseapp.com/login'}>New Sesh</a>
-    </FullScreenOverlay>
-  );
+	return(
+		<FullScreenOverlay>
+			<Illustration src = {swirl}/>
+			<h3>Spotify</h3>
+			<a href = {'https://damp-island-94750.herokuapp.com/login'}>New Sesh</a>
+		</FullScreenOverlay>
+	);
 };
 
 export default SpotifyReAuth;
