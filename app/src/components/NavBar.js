d0095396b20f12b1c52f7ecd52da1089af8dcc00
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-04-06 14:11:37 -0400

    Changed NavBar to functional component
    I'm not using state for navbar at the
    moment

--- app/src/components/NavBar.js
+++ app/src/components/NavBar.js
@@ -3,25 +3,15 @@ import {User} from 'react-feather';
 import {NavLink} from 'react-router-dom';
 
 import {Nav} from './StyledComps';
-
 import Logo from '../components/minorComps/Logo';
 
-class NavBar extends Component{
-  constructor(props){
-    super(props);
-
-    this.state = {
-      isExpanded: false
-    };
-  }
-  render(){
-    return(
-      <Nav>
-        <NavLink to='/'><Logo/></NavLink>
-        <NavLink to='/profile'><User color = 'black' size = {30}/></NavLink>
-      </Nav>
-    );
-  }
+const NavBar = (props) => {
+	return(
+		<Nav>
+			<NavLink to='/'><Logo/></NavLink>
+			<NavLink to='/profile'><User color = 'black' size = {30}/></NavLink>
+		</Nav>
+	);
 }
 
 export default (NavBar);
