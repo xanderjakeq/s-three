6e651263f6c60afa1d7c5792c3078c0acb5c77b1
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-04-06 14:16:10 -0400

    Refactored ProfilePage.js
    for readability, and indentation

--- app/src/components/ProfilePage.js
+++ app/src/components/ProfilePage.js
@@ -1,105 +1,101 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
+import {connect} from 'react-redux';
+import {withRouter} from 'react-router-dom';
 import axios from 'axios';
 
 import RadarChart from './RadarChart';
 import Track from './Track';
-import { DesktopFlex, ListContainer, Logout } from './StyledComps';
+import {DesktopFlex, ListContainer, Logout} from './StyledComps';
 
-import { logout, getTracks, getUserData } from '../actions';
+import {logout, getTracks, getUserData} from '../actions';
 
 class ProfilePage extends Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      fetchingLikedTracks: false,
-      likedTracksWithSpotifyData: [],
-      isReacting: false
-    };
-  }
+	state = {
+		fetchingLikedTracks: false,
+		likedTracksWithSpotifyData: [],
+		isReacting: false
+	};
 
-	componentDidMount() {
-			this.getLikedTracks()
+	render() {
+		if (!this.props.likedTracks) return null;
+		return (
+			<DesktopFlex>
+				<div style={{ flexGrow: '1' }}>
+					{this.props.userMusicTaste && (
+						<RadarChart audioFeatures={[this.props.userMusicTaste]} />
+					)}
+					<h1>Your Taste</h1>
+					{this.props.likedTracks.length === 0 && (
+						<p>get likin to develop your 'taste'</p>
+					)}
+					<Logout onClick={this.handleLogOut}>Signout</Logout>
+				</div>
+				<ListContainer>
+					<p>most recent likes</p>
+					{this.state.fetchingLikedTracks && <p>fetching liked tracks</p>}
+					{this.state.likedTracksWithSpotifyData.map(track => (
+						<Track
+							key={track.id}
+							trackData={track}
+						/>
+					))}
+				</ListContainer>
+			</DesktopFlex>
+		);
 	}
 
-  getLikedTracks = () => {
-    this.setState({
-      fetchingLikedTracks: true,
-      likedTracksWithSpotifyData: []
-    });
+	componentDidMount() {
+		this.getLikedTracks()
+	}
 
-    const { likedTracks } = this.props;
-    let likedTracksCopy;
+	getLikedTracks = () => {
+		this.setState({
+			fetchingLikedTracks: true,
+			likedTracksWithSpotifyData: []
+		});
 
-    if(likedTracks){
+		const {likedTracks} = this.props;
+		let likedTracksCopy;
 
-      likedTracksCopy = JSON.parse(JSON.stringify(likedTracks.reverse()));
-      let trackIds = likedTracksCopy.splice(0,50).map(track => track.id).join(',');
+		if(likedTracks){
+			likedTracksCopy = JSON.parse(JSON.stringify(likedTracks.reverse()));
+			let trackIds = likedTracksCopy.splice(0,50).map(track => track.id).join(',');
 
-      if (trackIds.length > 0) {
-        this.props.getTracks(trackIds, this.props.accessToken).then(res => {
-          if (!res) return;
+			if (trackIds.length > 0) {
+				this.props.getTracks(trackIds, this.props.accessToken).then(res => {
+					if (!res) return;
 
-          const updatedTracks = res.data.tracks.map((track, i) => {
-            return { ...track, ...likedTracks[i] };
-          });
+					const updatedTracks = res.data.tracks.map((track, i) => {
+						return { ...track, ...likedTracks[i] };
+					});
 
-          this.setState(() => ({
-            fetchingLikedTracks: false,
-            likedTracksWithSpotifyData: updatedTracks
-          }));
-        });
-      } else {
-        this.setState({
-          fetchingLikedTracks: false
-        });
+					this.setState(() => ({
+						fetchingLikedTracks: false,
+						likedTracksWithSpotifyData: updatedTracks
+					}));
+				});
+			} else {
+				this.setState({
+					fetchingLikedTracks: false
+				});
 
-      }
-    }
-  };
+			}
+		}
+	};
 
-  handleLogOut = () => {
-    this.props.history.push('/');
-    this.props.logout();
-  };
-
-  render() {
-    if (!this.props.likedTracks) return null;
-    return (
-      <DesktopFlex>
-        <div style={{ flexGrow: '1' }}>
-          {this.props.userMusicTaste && (
-            <RadarChart audioFeatures={[this.props.userMusicTaste]} />
-          )}
-          <h1>Your Taste</h1>
-          {this.props.likedTracks.length === 0 && (
-            <p>get likin to develop your 'taste'</p>
-          )}
-          <Logout onClick={this.handleLogOut}>Signout</Logout>
-        </div>
-        <ListContainer>
-          <p>most recent likes</p>
-          {this.state.fetchingLikedTracks && <p>fetching liked tracks</p>}
-          {this.state.likedTracksWithSpotifyData.map(track => (
-            <Track
-              key={track.id}
-              trackData={track}
-            />
-          ))}
-        </ListContainer>
-      </DesktopFlex>
-    );
-  }
+	handleLogOut = () => {
+		this.props.history.push('/');
+		this.props.logout();
+	};
 }
 
 const mstp = state => {
-  return {
-    likedTracks: state.auth.userData.likedTracks,
-    userMusicTaste: state.auth.userMusicTaste,
-    accessToken: state.track.accessToken
-  };
+	return {
+		likedTracks: state.auth.userData.likedTracks,
+		userMusicTaste: state.auth.userMusicTaste,
+		accessToken: state.track.accessToken
+	};
 };
 
 export default withRouter(connect(mstp, {logout, getTracks, getUserData})(ProfilePage));
