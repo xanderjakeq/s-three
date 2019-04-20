59cee753bed4eaf3650de94e6477d22f573aeccb
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-04-19 20:35:44 -0400

    Updated averaging

--- app/src/helpers.js
+++ app/src/helpers.js
@@ -3,15 +3,20 @@
  * @param {array} tracks 
  */
 export const averagePerObjectProperty = (tracks) => {
-    return tracks.reduce((taste, track) => {
+    const all = tracks.reduce((taste, track) => {
         Object.keys(track).forEach(key => {
             if(typeof track[key] !== 'number' || key === 'id') return //skip non numeric properties and id
             if(!taste[key]){
                 taste[key] = track[key]
             }else{
-                taste[key] = (taste[key] + track[key])/2
+                taste[key] = taste[key] + track[key]
             }
         })
         return taste
     }, {})
+
+    Object.keys(all).forEach(key => {
+        all[key] = all[key]/tracks.length 
+    })
+    return all
 }
